c50204d75e37707190d051ea9e8e73ebb281af68
Fenrir <skolleclair@gmail.com>
2025-04-23 16:02:03 +0900

    Change the background color of main.

--- perplexity/lib/main.dart
+++ perplexity/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:perplexity/pages/home_page.dart';
+import 'package:perplexity/theme/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: AppColors.background,
       ),
       home: HomePage(),
     );
