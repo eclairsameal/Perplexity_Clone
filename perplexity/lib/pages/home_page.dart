1f888db495d1d65eb5a0747b3a151d9daea271fc
Fenrir <skolleclair@gmail.com>
2025-04-30 14:25:26 +0900

    Complete the footer UI.

--- perplexity/lib/pages/home_page.dart
+++ perplexity/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:perplexity/theme/colors.dart';
 import 'package:perplexity/widgets/search_section.dart';
 import 'package:perplexity/widgets/side_bar.dart';
 
@@ -16,8 +17,77 @@ class HomePage extends StatelessWidget {
             child: Column(
               children: [
                 // search section
-                Expanded(child: SearchSection()),
-                Container(height: 20),
+                Expanded(
+                    child: SearchSection()
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pro",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Enterprise",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Store",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Blog",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Careers",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "English(English)",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 // footer
               ],
             ),
