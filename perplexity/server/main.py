43e41616fab6159b858aa3bd64cf1e1ce85f3972
Fenrir <skolleclair@gmail.com>
2025-05-07 10:52:43 +0900

    Implemented the function of searching the network and finding suitable sources

--- perplexity/server/main.py
+++ perplexity/server/main.py
@@ -20,7 +20,8 @@ def hello():
 def chat_endpoint(body: ChatBody):
     print(body.query)
     # search the web and find appropriate sources
-    search_service.web_search(body.query)
+    search_results = search_service.web_search(body.query)
+    print(search_results)
     # sort the sources
     # generate the response using LLM
     return body.query
--- perplexity/server/services/search_service.py
+++ perplexity/server/services/search_service.py
@@ -1,7 +1,6 @@
-
 from tavily import TavilyClient
-
 from services.config import Settings
+import trafilatura
 
 setting = Settings()
 tavily_client = TavilyClient(api_key=setting.TAVILY_API_KEY)
@@ -9,6 +8,15 @@ tavily_client = TavilyClient(api_key=setting.TAVILY_API_KEY)
 
 class SearchService:
     def web_search(self, query: str):
-        # print(Settings().TAVILY_API_KEY)
+        results = []
         response = tavily_client.search(query, max_results=10)
-        print(response.get("results", []))
+        search_results = response.get("results", [])
+        for result in search_results:
+            downloaded = trafilatura.fetch_url(result.get("url"))
+            content = trafilatura.extract(downloaded, include_comments=False)
+            results.append({
+                "title": result.get("title", ""),
+                "url": result.get("url", ""),
+                "content": content,
+            })
+        return results
